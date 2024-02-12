d78db6a1f45209b62649574742db6b6342236a7c
Harry <68599167+harrykeeran12@users.noreply.github.com>
2024-02-12 21:20:08 +0000

    feat: added initial mvn files.

---
+++ src/main/java/messenger/App.java
@@ -0,0 +1,13 @@
+package messager;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
