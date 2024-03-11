55c5038bfd1d0cd47698b895d7e67b0c7a0792de
Harry <68599167+harrykeeran12@users.noreply.github.com>
2024-03-11 16:58:44 +0000

    fix: added a verybasic command language.

---
+++ CGS_C/shared/Commands.java
@@ -0,0 +1,17 @@
+package CGS_C.shared;
+
+/**
+ * Commands:
+ * 
+ * This class stores the keyword commands for the server..
+ */
+public class Commands {
+
+  enum Commands{
+    REGISTER, LOGIN, LOGOUT
+  }
+  public Commands(){
+    return Commands;
+  }
+
+}
